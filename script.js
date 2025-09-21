0b548653f2b4a0babdd09be429e6579cc92bd239
Elizaveta <lisabeth.umarova@yandex.com>
2025-09-21 10:48:25 +0300

    done

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let massiv = [3, 6, 9, 2];
 function filter(arr, delet) {
   const result = [];
   for (const element of arr) {
-    if(delet(element)) {
+    if(!delet(element)) {
       result.push(element);
     }
   }
   return result;
 }
 
-const delet = (num) => num < 5;
+const delet = (num) => num > 5;
 
 console.log(filter(massiv, delet));
 
